cabc8ae37fdeb4f737f2649e7422b4181cca67ba
blaes1ng <carterblaesing@gmail.com>
2024-09-25 20:30:42 -0400

    an attempt of refactoring link entirely

---
+++ Sprint0/Classes/ILinkState.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint0.Classes
+{
+    public interface ILinkState
+    {
+        void Draw(SpriteBatch spriteBatch);
+        void setTextureIndex(int index);
+        void TakeDamage();
+        void Update();
+        void MoveUp();
+        void MoveDown();
+        void MoveLeft();
+        void MoveRight();
+        void UsePrimary();
+        void UseSecondary();
+    }
+}
