4da6486a39f3b9220a0b7edf1655ebc7fbb9886a
blaes1ng <carterblaesing@gmail.com>
2024-09-28 15:35:29 -0400

    sword rectangles

--- Sprint0/Classes/LinkSpriteFactory.cs
+++ Sprint0/Classes/LinkSpriteFactory.cs
@@ -44,19 +44,34 @@ namespace Sprint0.Classes
                 new Rectangle(86, 11, 16, 16),    // 5 animation frame 2 {up 2}
                 new Rectangle(35, 11, 16, 16),    // 6 animation frame 1 {left 1}  //gotta flip these horizontally --> did this
                 new Rectangle(52, 11, 16, 16),    // 7 animation frame 2 {left 2}
-                new Rectangle(94, 77, 15, 15),    // 8 Sword Right 1
-                new Rectangle(111, 78, 27, 14),   // 9 Sword Right 2
-                new Rectangle(139, 78, 23, 14),   // 10 Sword Right 3
-                new Rectangle(163, 77, 18, 15),   // 11 Sword Right 4
-                new Rectangle(94,109,16,16),      // 12 Sword Up 1                 //#12
-                new Rectangle(111,97,16,28),      // 13  Sword Up 2
-                new Rectangle(128,98,16, 28),     // 14 Sword up 3
-                new Rectangle(145,106,16, 19),    // 15 Sword up 4
-                new Rectangle(94,47,16,16),       // 16 Sword down 1
-                new Rectangle(111,47,16,27),      // 17 Sword down 2
-                new Rectangle(128,47,16,23),      // 18 Sword down 3
-                new Rectangle(145,47,16,19),      // 19 Sword down 4
-                new Rectangle(35,232,16,16)       // 20 hurt animation              
+                new Rectangle(107,11,16,16),      // 8 Link Down attack
+                new Rectangle (124, 11, 16, 16),  // 9 Link Right attack
+                new Rectangle (124, 11, 16, 16),  // 10 Link Left attack          //gotta flip these horizontally --> did this
+                new Rectangle (141, 11, 16, 16),   // 11 Link Up attack
+
+                new Rectangle (36,154,7,15),      //12 SWORD Up 1
+                new Rectangle (36,164,7,10),       //13 SWORD Up 2
+                new Rectangle (36,159,7,5),       //14 SWORD Up 3
+
+                new Rectangle (45,159,15,7),       //15 SWORD Right 1
+                new Rectangle (50,159,10,7),       //16 SWORD Right 2
+                new Rectangle (55,159,5,7),       //17 SWORD Right 3
+
+
+
+                //new Rectangle(94, 77, 15, 15),    // 8 Sword Right 1
+                //new Rectangle(111, 78, 27, 14),   // 9 Sword Right 2
+                //new Rectangle(139, 78, 23, 14),   // 10 Sword Right 3
+                //new Rectangle(163, 77, 18, 15),   // 11 Sword Right 4
+                //new Rectangle(94,109,16,16),      // 12 Sword Up 1                 //#12
+                //new Rectangle(111,97,16,28),      // 13  Sword Up 2
+                //new Rectangle(128,98,16, 28),     // 14 Sword up 3
+                //new Rectangle(145,106,16, 19),    // 15 Sword up 4
+                //new Rectangle(94,47,16,16),       // 16 Sword down 1
+                //new Rectangle(111,47,16,27),      // 17 Sword down 2
+                //new Rectangle(128,47,16,23),      // 18 Sword down 3
+                //new Rectangle(145,47,16,19),      // 19 Sword down 4
+                //new Rectangle(35,232,16,16)       // 20 hurt animation              
              };
 
             return sourceRectangles;
