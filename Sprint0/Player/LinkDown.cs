1854ca0dc71de0b899c0a171f58a9488101817d5
blaes1ng <carterblaesing@gmail.com>
2024-10-18 14:19:48 -0400

    door teleport bug fixed

--- Sprint0/Player/LinkDown.cs
+++ Sprint0/Player/LinkDown.cs
@@ -37,6 +37,8 @@ namespace Sprint0.Player
         }
         public void Update()
         {
+            CollideWall = false;
+
             wallBoundingBox = new Rectangle(0, (int)(144 * _link._scale.Y), (int)(256 * _link._scale.X), (int)(32 * _link._scale.Y));
             playerBoundingBox = GetScaledRectangle((int)_link._position.X, (int)_link._position.Y, 16, 16, _link._scale);
             if (playerBoundingBox.Intersects(wallBoundingBox))
