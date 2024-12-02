91ae2b1973a557cab1afb5db015bcfa85522b76c
Will Koran <willkoran26@outlook.com>
2024-12-02 11:10:44 -0500

    Further changes

--- Sprint0/Classes/KeyboardController.cs
+++ Sprint0/Classes/KeyboardController.cs
@@ -98,7 +98,10 @@ namespace Sprint0.Classes
                     }
                     else if (state.IsKeyDown(Keys.D1))
                     {
-                        _link.ArrowAttack();
+                        if (_link.inventory?.SelectedItem?.CurrentItemType == ItemType.bow)
+                        {
+                            _link.ArrowAttack();
+                        }
                     }
                     else if (state.IsKeyDown(Keys.D2))
                     {
@@ -106,7 +109,10 @@ namespace Sprint0.Classes
                     }
                     else if (state.IsKeyDown(Keys.D3))
                     {
-                        _link.UseBomb();
+                        if (_link.inventory?.SelectedItem?.CurrentItemType == ItemType.boom)
+                        {
+                            _link.UseBomb();
+                        }
                     }
                     else if (state.IsKeyDown(Keys.Space))
                     {
